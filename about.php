a28aca9fb665c67c959fc315a16e5c35e6961bd8
SamratBhandari <106598415+SamratBhandari@users.noreply.github.com>
2022-05-31 18:19:43 +0545

    Add files via upload

---
+++ about.php
@@ -0,0 +1,61 @@
+<?php include 'header.php';?>
+
+
+<!-- Content -->
+<div id="content">
+
+    <!-- Intro -->
+    <section class="p-t-b-150">
+        <div class="container">
+            <div class="intro-main">
+                <div class="row">
+
+                    <!-- Intro Detail -->
+                    <div class="col-md-7">
+                        <div class="text-sec padding-right-0">
+                            <h5>Health Check Ups</h5>
+                            <p>This hospital has been working for over a decade now,
+                               The hospital provides medical and surgical treatments. It current has a capacity of 535 beds.
+                               It began as an idea of a patient-oriented, socially responsible center, making superior
+                               quality healthcare services convenient and accessible for the community it serves
+                               It aims to meet all healthcare needs of the nation through unparalleled patient care and wellness programs.</p>
+                            <ul class="row">
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h6>
+                                    <p>For emergency cases we provide the doctor who is good in every field,
+                                        And for further checkup case related doctors will arrive shortly </p>
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> QUALIFIED DOCTORS</h6>
+                                    <p> PandS Hospital have the best doctors from all the Department,
+                                        and they have gained excellent reviews and patient's trust  </p>
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> ONLINE APPOINTMENT</h6>
+                                    <p> You can either contact us or login to our website for ONLINE APPOINTMENT,
+                                        Visiting hospital will be better if you,re new. </p>
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
+                                    <p>We will be very happy to help you as a service giver,
+                                        So, you can also contact us to share the details of your problems </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- Intro Timing -->
+                    <div class="col-md-5"> <img class="img-responsive intro-img" src="images/intro-img.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</div>
+
+<!-- Footer -->
+<!--======= FOOTER =========-->
+
+<?php include 'footer.php';?>
